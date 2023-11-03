706839205ba5391034660ecde48d4c355257b629
qq8244353 <qqq000a@outlook.com>
2023-11-03 23:02:50 +0900

    add ln script

--- ln-script.sh
+++ ln-script.sh
@@ -1,3 +1,5 @@
 # ln -s $(pwd)/tmux.conf ~/.tmux.conf
 # ln -s $(pwd)/gh-dash.yml ~/.config/gh-dash/config.yml
-ln -s $(pwd)/polybar/config ~/.config/polybar/config
+# ln -s $(pwd)/polybar/config ~/.config/polybar/config
+mkdir -p ~/.config/alacritty
+ln -s $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
