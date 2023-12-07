f471fce47fc2765f2a7307afee4467558b4ffe27
qq8244353 <qqq000a@outlook.com>
2023-12-07 19:15:58 +0900

    rm ln-script

--- ln-script.sh
+++
@@ -1,5 +0,0 @@
-# ln -s $(pwd)/tmux.conf ~/.tmux.conf
-# ln -s $(pwd)/gh-dash.yml ~/.config/gh-dash/config.yml
-# ln -s $(pwd)/polybar/config ~/.config/polybar/config
-mkdir -p ~/.config/alacritty
-ln -s $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
