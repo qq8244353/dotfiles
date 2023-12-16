1a8fe55a8f61ac49bba5bd6f63499c936ea79891
qq8244353 <qqq000a@outlook.com>
2023-12-17 02:34:11 +0900

    complete docker install script

--- ubuntu-zsh.sh
+++ ubuntu-zsh.sh
@@ -134,6 +134,8 @@ if false; then
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 fi
 exit 0
 
