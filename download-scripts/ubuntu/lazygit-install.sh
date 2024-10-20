ceaafefef52b5891d5c5375c7a8816ec653c63fd
qq8244353 <qqq000a@outlook.com>
2024-10-20 19:20:17 +0900

    lazygit install script update

--- download-scripts/ubuntu/lazygit-install.sh
+++ download-scripts/ubuntu/lazygit-install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 set -eux
-TMP_DIR=$(mktemp -d)
-cd ${TMP_DIR}
-trap "rm -rf ${TMP_DIR}" EXIT
 
-wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
+mkdir -p binaries
+cd binaries
+if [ ! -e "lazygit_0.40.2_Linux_x86_64.tar.gz" ]; then
+  wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
+fi
 tar xzf lazygit_0.40.2_Linux_x86_64.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+mkdir -p bin
+mv lazygit bin/lazygit
+# sudo install lazygit /usr/local/bin
+echo 'export PATH=$PATH:~/.config/dotfiles/download-scripts/ubuntu/binaries/bin' >> ~/.zshrc
