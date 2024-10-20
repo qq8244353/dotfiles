1acf2fa0b004c0cb04e384bddb1cad64337410cb
qq8244353 <qqq000a@outlook.com>
2024-10-20 17:59:40 +0900

    インストールスクリプトアップデート

--- download-scripts/ubuntu/nvim-install.sh
+++ download-scripts/ubuntu/nvim-install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -eux
-TMP_DIR=$(mktemp -d)
-cd ${TMP_DIR}
-trap "rm -rf ${TMP_DIR}" EXIT
 
-# wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-# tar xzf nvim-linux64.tar.gz
-# sudo cp -r ./nvim-linux64/* /usr/
+mkdir -p binaries
+cd binaries
+# wget -P binaries https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+if [ ! -e "nvim-linux64.tar.gz" ]; then
+  wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+fi
+tar xzf nvim-linux64.tar.gz
 # sudo update-alternatives --config editor
 # sudo update-alternatives --install /usr/bin/editor editor $(which nvim) 50
 echo 'export EDITOR=nvim' >> ~/.zshrc
+echo 'export PATH=$PATH:~/.config/dotfiles/download-scripts/ubuntu/binaries/nvim-linux64/bin' >> ~/.zshrc
+
