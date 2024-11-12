8540479834019ede3fad244f5838411c5ab37f1e
qq8244353 <qqq000a@outlook.com>
2024-11-12 09:08:37 +0900

    ポモドーロコマンド追加

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,3 +6,6 @@ function pps() {
 function ppsv() {
 	paps --separation-lines --header --header-center=$2 --header-left=$3 --hyphens --left-margin=18 --top-margin=18 --right-margin=18 --bottom-margin=18 --font 8 $1
 }
+alias ganbaruzo='nohup ~/.config/dotfiles/script/notify.sh 1500 "25分経過〜お疲れ様！" "ポモドーロ" &'
+alias kyuukei='nohup ~/.config/dotfiles/script/notify.sh 300 "休憩終了〜切り替えて頑張ろう！" "ポモドーロ" &'
+alias sagyoumae='nohup ~/.config/dotfiles/script/notify.sh 900 "作業開始しよう！" "ポモドーロ" &'
